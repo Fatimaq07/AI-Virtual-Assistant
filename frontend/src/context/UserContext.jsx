b47f92a3eab3561c0efef34abddcae072f793834
FATIMA QURESHI <qfatima504@gmail.com>
2025-10-02 15:25:45 +0530

    front end connection with backend

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -5,7 +5,7 @@ export const userDataContext = createContext()
 
 function UserContext({ children }) {
   // Use .env variable if available, otherwise fallback to localhost
-  const serverUrl = import.meta.env.VITE_SERVER_URL || "http://localhost:8000"
+  const serverUrl = "https://ai-virtual-assistant0-backend.onrender.com"
 
   const [userData, setUserData] = useState(null)
   const [frontendImage, setFrontendImage] = useState(null)
